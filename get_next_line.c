52a28b4706e864cdc32a1e84295bb49fd063a68d
jules <julesp415@gmail.com>
2020-11-18 17:25:24 +0100

    Added header and fix norme

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/18 17:23:26 by jpeyron           #+#    #+#             */
+/*   Updated: 2020/11/18 17:24:53 by jpeyron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 #include <sys/types.h>
@@ -17,7 +29,7 @@ void	extract_line(int fd, char **line, char **content, int ret)
 {
 	int		pos;
 	char	*tmp;
-	
+
 	pos = strfind_nl(content[fd], 1);
 	if (content[fd][pos] == '\n')
 	{
@@ -58,7 +70,7 @@ int		get_next_line(int fd, char **line)
 		free(content[fd]);
 		content[fd] = str;
 		if (strfind_nl(content[fd], 0) != -1)
-			break;
+			break ;
 	}
 	if (ret < 0)
 		return (-1);
@@ -66,14 +78,4 @@ int		get_next_line(int fd, char **line)
 		return (0);
 	extract_line(fd, line, content, ret);
 	return (1);
-}
-
-int main()
-{
-	int 	fd = open("1.txt", O_RDONLY);
-	char	*line;
-
-	while (get_next_line(fd, &line) > 0)
-		printf("%s\n", line);
-	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/18 17:23:36 by jpeyron           #+#    #+#             */
+/*   Updated: 2020/11/18 17:25:05 by jpeyron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 64
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
-
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*ft_strnew(size_t size);
 char	*ft_strdup(const char *s);
@@ -17,4 +28,3 @@ int		strfind_nl(char const *s, int zero);
 int		get_next_line(int fd, char **line);
 
 #endif
-
